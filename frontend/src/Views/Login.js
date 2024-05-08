757d951bc6083fbe640467a54d4c1f989cd139f2
Luke <lucoin.zh@gmail.com>
2024-05-08 10:36:43 +0800

    update the get_User_Info

--- frontend/src/Views/Login.js
+++ frontend/src/Views/Login.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import {
   Container,
   TextField,
@@ -43,12 +43,12 @@ function LogInPage() {
     switch (name) {
       case "email":
         errors.email = value.includes("@umass.edu")
-            ? ""
-            : 'Email should contain an "@umass.edu"';
+          ? ""
+          : 'Email should contain an "@umass.edu"';
         break;
       case "password":
         errors.password =
-            value.length < 8 ? "Password must be at least 8 characters long" : "";
+          value.length < 8 ? "Password must be at least 8 characters long" : "";
         break;
       default:
         break;
@@ -68,17 +68,17 @@ function LogInPage() {
       console.log("Form Data:", formData);
       try {
         const response = await dispatch(
-            fetchLogin({
-              email: formData.email,
-              password: formData.password,
-            })
+          fetchLogin({
+            email: formData.email,
+            password: formData.password,
+          })
         ); // 使用unwrap来处理Redux Toolkit的异步Thunk
 
         console.log("Login Response:", response); // 日志输出查看响应内容
 
         // 假设后端返回的是这种格式 { data: { access_token: 'token_here' } }
-        console.log(token)
-        console.log("-------")
+        console.log(token);
+        console.log("-------");
 
         if (token !== "") {
           setSnackbarMessage("Login successfully!");
@@ -91,7 +91,6 @@ function LogInPage() {
           setSnackbarSeverity("error");
           setOpenSnackbar(true); // 显示Snackbar
         }
-
       } catch (error) {
         console.error("Login Error:", error);
         setSnackbarMessage("An unexpected error occurred");
@@ -124,89 +123,89 @@ function LogInPage() {
   };
 
   return (
-      <div style={backgroundStyle}>
-        <div style={{ width: "100%", textAlign: "center" }}>
-          <div className="jacquard-24-regular" style={{ marginTop: -220 }}>
-            <span style={{ fontSize: 80, color: "white" }}>Dream Singer </span>
-          </div>
+    <div style={backgroundStyle}>
+      <div style={{ width: "100%", textAlign: "center" }}>
+        <div className="jacquard-24-regular" style={{ marginTop: -220 }}>
+          <span style={{ fontSize: 80, color: "white" }}>Dream Singer </span>
         </div>
-        <Container component={Paper} elevation={6} style={boxStyle}>
-          <div
-              style={{
-                marginTop: 8,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-          >
-            <Typography component="h1" variant="h5">
-              Log In
-            </Typography>
-            <form style={{ width: "100%", marginTop: 3 }} onSubmit={handleSubmit}>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <TextField
-                      autoComplete="username"
-                      name="email"
-                      required
-                      fullWidth
-                      id="email"
-                      label="Email"
-                      autoFocus
-                      value={formData.email}
-                      onChange={handleChange}
-                      error={!!formData.errors.email}
-                      helperText={formData.errors.email}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                      required
-                      fullWidth
-                      name="password"
-                      label="Password"
-                      type="password"
-                      id="password"
-                      value={formData.password}
-                      onChange={handleChange}
-                      error={!!formData.errors.password}
-                      helperText={formData.errors.password}
-                  />
-                </Grid>
-              </Grid>
-              <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  style={{ marginTop: 10, marginBottom: 5 }}
-              >
-                Log In
-              </Button>
-              <Grid container justifyContent="flex-end">
-                <Grid item>
-                  <Link component={RouterLink} to="/signup" variant="body2">
-                    Do not have an account yet? Sign Up
-                  </Link>
-                </Grid>
-              </Grid>
-            </form>
-          </div>
-        </Container>
-        <Snackbar
-            open={openSnackbar}
-            autoHideDuration={6000}
-            onClose={() => setOpenSnackbar(false)}
-        >
-          <Alert
-              onClose={() => setOpenSnackbar(false)}
-              severity={snackbarSeverity}
-              sx={{ width: "100%" }}
-          >
-            {snackbarMessage}
-          </Alert>
-        </Snackbar>
       </div>
+      <Container component={Paper} elevation={6} style={boxStyle}>
+        <div
+          style={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Log In
+          </Typography>
+          <form style={{ width: "100%", marginTop: 3 }} onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="username"
+                  name="email"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email"
+                  autoFocus
+                  value={formData.email}
+                  onChange={handleChange}
+                  error={!!formData.errors.email}
+                  helperText={formData.errors.email}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  error={!!formData.errors.password}
+                  helperText={formData.errors.password}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              style={{ marginTop: 10, marginBottom: 5 }}
+            >
+              Log In
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link component={RouterLink} to="/signup" variant="body2">
+                  Do not have an account yet? Sign Up
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Container>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={() => setOpenSnackbar(false)}
+      >
+        <Alert
+          onClose={() => setOpenSnackbar(false)}
+          severity={snackbarSeverity}
+          sx={{ width: "100%" }}
+        >
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
+    </div>
   );
 }
 
