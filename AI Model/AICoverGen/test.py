a5bfb947ba2f118c28938b125290823f4ba94207
Liang-Yu Chen <58724609+liangyu0516@users.noreply.github.com>
2024-05-11 23:52:41 -0400

    Delete AI Model directory

--- AI Model/AICoverGen/test.py
+++
@@ -1,8 +0,0 @@
-from apis import *
-
-song_input, rvc_dirname, output_path, pitch_change = "billie_jean.ogg", "MJOTW", "./debug_output", 0
-#generate_song(song_input, rvc_dirname, output_path, pitch_change)
-res = preprocess(song_input, "preprocessed", "billie")
-
-for r in res:
-    print(r)
